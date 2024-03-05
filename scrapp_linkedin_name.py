71ff243f9c660a734fd11e1f9f7867a4766a59a7
bharatnokhawal <138272054+bharatnokhawal@users.noreply.github.com>
2024-03-05 18:15:06 +0530

    Update scrapp_linkedin_name.py

--- scrapp_linkedin_name.py
+++ scrapp_linkedin_name.py
@@ -1,3 +1,4 @@
+# import all the libraries like csv , selenium , BeautifulSoup
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,12 +11,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.maximize_window()
-
+#define a funtion for login purpose 
+#define a funtion for login purpose 
 def login():
     driver.get("https://www.linkedin.com/login")
     time.sleep(1)
-    email = "bharatnokhawal201920@gmail.com"  # Replace with your LinkedIn email
-    password = "Bharat@456"  # Replace with your LinkedIn password
+    email = "bharatnokhawal201920@gmail.com"  #  LinkedIn email
+    password = "Bharat@456"  #   password
 
     driver.find_element(By.ID, "username").send_keys(email)
     driver.find_element(By.ID, "password").send_keys(password)
@@ -23,7 +25,7 @@ def login():
     time.sleep(3)
     driver.get("https://www.linkedin.com/feed/")
     time.sleep(5)
-
+#search using first name and last name like fo search box and search Bharat kumawat and enter
 def search_user(first_name, last_name):
     driver.get("https://www.linkedin.com/search/results/people/")
     time.sleep(2)
@@ -32,7 +34,7 @@ def search_user(first_name, last_name):
     search_bar.send_keys(first_name + " " + last_name)
     search_bar.send_keys(Keys.RETURN)
     time.sleep(3)
-
+#go to all search name one by one store in to the csv file (first 10)
 def scrape_profile_data():
     profiles = []
     try:
@@ -67,8 +69,8 @@ def scrape_profile_data():
 
 if __name__ == "__main__":
     login()
-    first_name = "Bharat"  # Enter the first name of the user you want to search
-    last_name = "kumawat"  # Enter the last name of the user you want to search
+    first_name = "Bharat"  # the first name of the user you want to search
+    last_name = "kumawat"  # the last name of the user you want to search
     search_user(first_name, last_name)
     scrape_profile_data()
     print("Data scraped and stored in linkedin_profiles.csv")
